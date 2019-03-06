572796b09fc099adcc2892bba8fe39fe4c331812
Alistair Johnson <alistairewj@gmail.com>
2019-03-06 12:13:42 -0500

    better filter out prophylactic/prn, and update start/stop offset rules

--- sql/cefepime.sql
+++ sql/cefepime.sql
@@ -1,35 +1,40 @@
-
-cefepime_pt AS (
-  SELECT DISTINCT 
-    CASE 
-        WHEN drugstartoffset <= drugstopoffset THEN drugstartoffset
-        WHEN drugstopoffset < drugstartoffset THEN drugstopoffset
-    END AS drugstartoffset,
-    CASE 
-        WHEN drugstartoffset <= drugstopoffset THEN drugstopoffset
-        WHEN drugstopoffset < drugstartoffset THEN drugstartoffset
-    END AS drugstopoffset,
-    patientunitstayid
-  FROM `physionet-data.eicu_crd.medication` 
-  WHERE (drughiclseqno IN (10132,35848,37021)
-  OR LOWER(drugname) LIKE '%cefepim%'
-  OR LOWER(drugname) LIKE '%maxipim%')
-  AND routeadmin IN (
-    'IV', 
-    'Intravenous', 
-    'INTRAVENOU', 
-    'INTRAVEN', 
-    'IntraVENOUS', 
-    'IV (intravenous)                                                                                    ', 
-    'INTRAVENOUS',
-    'IV - brief infusion (intravenous)                                                                   ',
-    'PERIPH IV',
-    'IV Push'
-  )
-  AND drugordercancelled = 'No'
-  AND prn = 'No'
-  AND patientunitstayid IN (
-      SELECT patientunitstayid FROM `physionet-data.eicu_crd.patient` 
-      WHERE hospitalid IN (SELECT hospitalid FROM hospitals_tb)
-    )
-),
+SELECT 
+  patientunitstayid,
+  CASE 
+      WHEN drugstopoffset = 0 THEN drugstartoffset
+      WHEN drugstartoffset <= drugstopoffset THEN drugstartoffset
+      WHEN drugstopoffset < drugstartoffset THEN drugstopoffset
+  END AS drugstartoffset,
+  CASE 
+      WHEN drugstopoffset = 0 THEN NULL
+      WHEN drugstartoffset <= drugstopoffset THEN drugstopoffset
+      WHEN drugstopoffset < drugstartoffset THEN drugstartoffset
+  END AS drugstopoffset,
+  drugorderoffset,
+  m.frequency,
+  map.classification,
+  dosage
+FROM `physionet-data.eicu_crd.medication` m
+LEFT JOIN `lcp-internal.vanco.medication_frequency_map` map
+  on m.frequency = map.frequency
+WHERE 
+  (drughiclseqno IN (10132,35848,37021) OR LOWER(drugname) LIKE '%cefepim%' OR LOWER(drugname) LIKE '%maxipim%')
+AND routeadmin IN (
+  'IV', 
+  'Intravenous', 
+  'INTRAVENOU', 
+  'INTRAVEN', 
+  'IntraVENOUS', 
+  'IV (intravenous)                                                                                    ', 
+  'INTRAVENOUS',
+  'IV - brief infusion (intravenous)                                                                   ',
+  'PERIPH IV',
+  'IV Push'
+)
+AND drugordercancelled = 'No'
+AND prn = 'No'
+AND map.classification NOT IN
+(
+  'TPN', 'dialysis', 'prophylactic'
+)
+ORDER BY patientunitstayid, drugstartoffset;
