7a0e54e24344621f2a7c42c2ab3089e22f981c09
Alistair Johnson <alistairewj@gmail.com>
2019-08-06 11:43:09 -0400

    pivot creatinine and merge data across a hospitalization

--- sql/pivoted_creatinine.sql
+++ sql/pivoted_creatinine.sql
@@ -1,20 +1,13 @@
-DROP TABLE IF EXISTS vanco.pivoted_creatinine CASCADE;
-CREATE TABLE vanco.pivoted_creatinine as
--- remove duplicate labs if they exist at the same time
-with vw0 as
-(
-  select
-      patientunitstayid
-    , labname
-    , labresultoffset
-    , labresultrevisedoffset
-  from lab
-  where labname = 'creatinine'
-  group by patientunitstayid, labname, labresultoffset, labresultrevisedoffset
-  having count(distinct labresult)<=1
-)
+-- get all creatinines for a single patientunitstayid
+DROP TABLE IF EXISTS vanco.creatinine_stg CASCADE;
+CREATE TABLE vanco.creatinine_stg AS
+select
+  pt.patienthealthsystemstayid
+  , pt.patientunitstayid
+  , labresultoffset as chartoffset
+  , labresult as creatinine
+from 
 -- get the last lab to be revised
-, vw1 as
 (
   select
       lab.patientunitstayid
@@ -28,20 +21,32 @@ with vw0 as
           ORDER BY lab.labresultrevisedoffset DESC
         ) as rn
   from lab
-  inner join vw0
-    ON  lab.patientunitstayid = vw0.patientunitstayid
-    AND lab.labname = vw0.labname
-    AND lab.labresultoffset = vw0.labresultoffset
-    AND lab.labresultrevisedoffset = vw0.labresultrevisedoffset
-  -- only valid lab values
   WHERE 
     (lab.labname = 'creatinine' and lab.labresult >= 0.1 and lab.labresult <= 28.28)
-)
-select
-    patientunitstayid
-  , labresultoffset as chartoffset
-  , MAX(case when labname = 'creatinine' then labresult else null end) as creatinine
-from vw1
+) vw1
+INNER JOIN patient pt
+  ON vw1.patientunitstayid = pt.patientunitstayid
 where rn = 1
-group by patientunitstayid, labresultoffset
-order by patientunitstayid, labresultoffset;
+ORDER BY patienthealthsystemstayid, patientunitstayid, chartoffset;
+
+-- copy lab measurements so that out of ICU labs occur for each patientunitstayid
+-- e.g. if there are two ICU stays in one hosp, this makes sure creatinine data
+-- across the entire hospitalization is stored with the patientunitstayid for both ICU stays
+-- patienthealthsystemstayid == 86 is a good test case
+DROP TABLE IF EXISTS vanco.pivoted_creatinine CASCADE;
+CREATE TABLE vanco.pivoted_creatinine as
+-- remove duplicate labs if they exist at the same time
+SELECT
+  pt_h.patienthealthsystemstayid
+  , pt_h.patientunitstayid
+  -- adjust chartoffset to match the current patient
+  , cr.chartoffset + (pt.hospitaldischargeoffset - pt_h.hospitaldischargeoffset) as chartoffset
+  , cr.creatinine
+FROM vanco.creatinine_stg cr
+-- get the patient's current hosp disch offset
+INNER JOIN patient pt
+  ON cr.patientunitstayid = pt.patientunitstayid
+-- now associate all creatinine measurements with a given hospitalization  
+INNER JOIN patient pt_h
+  ON cr.patienthealthsystemstayid = pt_h.patienthealthsystemstayid
+ORDER BY cr.patienthealthsystemstayid, cr.patientunitstayid, chartoffset;
