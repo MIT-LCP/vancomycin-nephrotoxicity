93388c650337aaf52d4ecdc938cec35ab9296da7
Alistair Johnson <alistairewj@gmail.com>
2020-07-22 08:47:17 -0400

    remove old bigquery code

--- sql-gcp/negative_group.sql
+++
@@ -1,9 +0,0 @@
-SELECT patientunitstayid FROM `physionet-data.eicu_crd.intakeoutput`  
-WHERE LOWER(celllabel) NOT LIKE '%vanco%'
-AND LOWER(cellpath) NOT LIKE '%vanco%'
-UNION DISTINCT
-SELECT patientunitstayid FROM `physionet-data.eicu_crd.lab`  
-WHERE LOWER(labname) NOT LIKE '%vanco%'
-UNION DISTINCT
-SELECT patientunitstayid FROM `physionet-data.eicu_crd.medication`   
-WHERE LOWER(drugname) NOT LIKE '%vanco%'
